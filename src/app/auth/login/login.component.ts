d4dc4ad7b53b07a4cea0fbee5594d3fa1ceefa20
Daniel Goins <dwgoins98@gmail.com>
2025-02-20 16:17:55 -0500

    Handling email and password inputs in the login form using template driven forms.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 
+import { FormsModule, NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  
+  // Template Driven approach
+
+  onSubmit(formData: NgForm) {
+
+    const eneteredEmail: string = formData.value.email;
+    const enteredPassword: string = formData.value.password;
+
+    console.log("Email: " + eneteredEmail);
+    console.log("Password: " + enteredPassword);
+  }
 }
