e6418efc32fb082efa7444f14c6ef9234e22fc90
Daniel Goins <dwgoins98@gmail.com>
2025-02-21 19:22:17 -0500

    Using Reactive Forms:     - Updated login component to include a FormGroup with 2 FormControl properties     - Updated login template to show conditional error statements if getters return false

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,45 @@
-import {
-  afterNextRender,
-  Component,
-  DestroyRef,
-  viewChild,
-} from '@angular/core';
+import { Component } from '@angular/core';
 
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime, Subscription } from 'rxjs';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    emailControl: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    passwordControl: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  public get emailIsInvalid(): boolean {
+    return (
+      this.form.controls.emailControl.touched &&
+      this.form.controls.emailControl.dirty &&
+      this.form.controls.emailControl.invalid
+    );
+  }
+
+  public get passwordIsInvalid(): boolean {
+    return (
+      this.form.controls.passwordControl.touched &&
+      this.form.controls.passwordControl.dirty &&
+      this.form.controls.passwordControl.invalid
+    );
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
+}
