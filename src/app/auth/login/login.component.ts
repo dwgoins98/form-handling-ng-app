226a858fc8d3aa5be95dc74a8dd206b53a448a64
Daniel Goins <dwgoins98@gmail.com>
2025-02-21 19:52:29 -0500

    fix: update login component to improve form handling
    - Improved form validation logic
    - Enhanced error handling for user inputs

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 import {
   AbstractControl,
@@ -7,6 +7,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
 
 function mustContainAmpersand(control: AbstractControl) {
   if (control.value.includes('&')) {
@@ -16,19 +17,33 @@ function mustContainAmpersand(control: AbstractControl) {
   return { doesNotContainAmpersand: true };
 }
 
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'admin@example.com') {
+    return of(null);
+  }
+  return of({ emailNotUnique: true });
+}
+
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
   form = new FormGroup({
     emailControl: new FormControl('', {
       validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique],
     }),
     passwordControl: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6), mustContainAmpersand],
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        mustContainAmpersand,
+      ],
     }),
   });
 
@@ -46,6 +61,32 @@ export class LoginComponent {
       this.form.controls.passwordControl.dirty &&
       this.form.controls.passwordControl.invalid
     );
+  }
+
+  ngOnInit(): void {
+    const savedForm = window.localStorage.getItem('saved-login-form');
+
+    if (savedForm) {
+      const loadedForm = JSON.parse(savedForm);
+      this.form.patchValue({
+        emailControl: loadedForm.email,
+      });
+    }
+
+    const formSubscription = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.emailControl })
+          );
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      formSubscription.unsubscribe();
+    });
   }
 
   onSubmit() {
