7ca25f761375ed993f25bad27dfb9722e2e600b0
Daniel Goins <dwgoins98@gmail.com>
2025-02-21 20:07:24 -0500

    Working on signup sheet     - Made it identical to login form but less restrictive     - Updated signup component to be a Reactive Forms     - Linked template with component

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+  private destroyRef = inject(DestroyRef);
 
+  form = new FormGroup({
+    emailControl: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    passwordControl: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  public get emailIsInvalid(): boolean {
+    return (
+      this.form.controls.emailControl.touched &&
+      this.form.controls.emailControl.dirty &&
+      this.form.controls.emailControl.invalid
+    );
+  }
+
+  public get passwordIsInvalid(): boolean {
+    return (
+      this.form.controls.passwordControl.touched &&
+      this.form.controls.passwordControl.dirty &&
+      this.form.controls.passwordControl.invalid
+    );
+  }
+
+  ngOnInit(): void {
+    const savedForm = window.localStorage.getItem('saved-login-form');
+
+    if (savedForm) {
+      const loadedForm = JSON.parse(savedForm);
+      this.form.patchValue({
+        emailControl: loadedForm.email,
+      });
+    }
+
+    const formSubscription = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.emailControl })
+          );
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      formSubscription.unsubscribe();
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
+
+  onReset() {
+    this.form.reset()
+  }
 }
