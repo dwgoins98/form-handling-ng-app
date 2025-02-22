1136b580e9d41c758eeaa8343966383d0e0036eb
Daniel Goins <dwgoins98@gmail.com>
2025-02-21 19:32:28 -0500

    Created a custom validator

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+
+function mustContainAmpersand(control: AbstractControl) {
+  if (control.value.includes('&')) {
+    return null;
+  }
+
+  return { doesNotContainAmpersand: true };
+}
 
 @Component({
   selector: 'app-login',
@@ -19,7 +28,7 @@ export class LoginComponent {
       validators: [Validators.email, Validators.required],
     }),
     passwordControl: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)],
+      validators: [Validators.required, Validators.minLength(6), mustContainAmpersand],
     }),
   });
 
